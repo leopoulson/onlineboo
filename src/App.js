0bd607c7307a345939ebd42403f3a948562393de
Leo Poulson <lp16860@my.bristol.ac.uk>
2018-07-20 20:22:48 +0100

    add buttons for fb, about, mixlr

--- src/App.js
+++ src/App.js
@@ -9,6 +9,11 @@ class App extends Component {
         <div className="Content">
           <div className="SideBar">
             <img src={boopic} className="LogoStyle" alt="boo"/>
+            <div id="buttons" className="ButtonStyle">
+              <button id="streamButton" className="button">stream</button>
+              <button id="aboutButton" className="button">about</button>
+              <button id="facebookButton" className="button">facebook</button>
+            </div>
           </div>
           <div className="ShowBar">
           </div>
