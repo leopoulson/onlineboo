e7ddedbe647547c89571c92f2bb586eeeb5223a8
Leo Poulson <lp16860@my.bristol.ac.uk>
2018-07-20 20:40:04 +0100

    gave buttons function

--- src/Buttons.js
+++ src/Buttons.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 
 class Buttons extends Component {
+    streamButtonClicked () {
+        window.open('https://www.mixlr.com/planetboo', '_blank');
+    }
+
+    aboutButtonClicked () {
+
+    }
+
+    facebookButtonClicked () {
+        window.open('https://www.facebook.com/booplanet2/', '_blank');
+    }
 
     render() {
         return <div id="buttons" className="ButtonStyle">
-                  <button id="streamButton" className="button">stream</button>
-                  <button id="aboutButton" className="button">about</button>
-                  <button id="facebookButton" className="button">facebook</button>
+                  <button id="streamButton" className="button" onClick={this.streamButtonClicked}>stream</button>
+                  <button id="aboutButton" className="button" onClick={this.aboutButtonClicked}>about</button>
+                  <button id="facebookButton" className="button" onClick={this.facebookButtonClicked}>facebook</button>
                </div>
     }
 }
