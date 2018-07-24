1e64bc5dc36fb596dcd23c6de27ad6021b98381e
Leo Poulson <lp16860@my.bristol.ac.uk>
2018-07-24 22:01:07 +0100

    remove excess rubbish

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
 import React, { Component } from 'react';
 import boopic from './boopic.jpg';
 import Buttons from './Buttons.js';
-import Mixcloud from './Mixcloud.js';
 import './App.css';
 
 class App extends Component {
-  mixcloudPlayClicked() {
-    //Mixcloud.load("https://www.mixcloud.com/planetboo/planet-boo-late-nite/");
-    Mixcloud.load();
-  }
-
   render() {
-    //this.player = Mixcloud.Mixcloud;
     return (
       <div className="App">
         <div className="Content">
           <div className="SideBar">
             <img src={boopic} className="LogoStyle" alt="boo"/>
             <Buttons/>
-            <button className="button" onClick={this.mixcloudPlayClicked}>mixcloud-play</button>
-           
           </div>
           <div className="ShowBar">
           </div>
+          <div data-mixcloud-play-button="/planetboo/planet-bootyshakers/">Click 4 bootyshakers</div>
         </div>
       </div>
     );
