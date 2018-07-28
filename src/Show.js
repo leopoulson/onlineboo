2b7da64203e4ef2f5cafdc9ace900df60c125ffd
Leo Poulson <lp16860@my.bristol.ac.uk>
2018-07-28 21:54:37 +0100

    add Show class

---
+++ src/Show.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Show extends Component {
+    render () {
+        return null;
+    }
+}
+export default Show;
--- src/Shows.js
+++ src/Shows.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Show from './Show.js'
 import './App.css';
 
 class Shows extends Component {
