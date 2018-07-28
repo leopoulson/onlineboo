19bcc99ac2bffcdfca96e008d5ff7136c854e461
Leo Poulson <lp16860@my.bristol.ac.uk>
2018-07-28 21:37:50 +0100

    render show

--- src/App.js
+++ src/App.js
@@ -5,42 +5,23 @@ import Shows from './Shows.js';
 import './App.css';
 
 class App extends Component {
-  getResponse() {
-    //so the API hook is this https://api.mixcloud.com/planetboo/cloudcasts/
-    var xmlHttp = new XMLHttpRequest();
-    var url = "https://api.mixcloud.com/planetboo/cloudcasts/";
-    xmlHttp.open("GET", url, false);
-    xmlHttp.send(null);
-    return(xmlHttp.responseText);
-  }
 
-  getUploads() {
-    var response = this.getResponse();
-    var responseParsed = JSON.parse(response);
-    var shows = responseParsed.data;
-
-    for (var i = 0; i < shows.length; i++) {
-      console.log(shows[i]);
+    render() {
+        return (
+            <div className="App">
+                <div className="Content">
+                    <div className="SideBar">
+                        <img src={boopic} className="LogoStyle" alt="boo"/>
+                        <Buttons/>
+                    </div>
+                    <div className="ShowBar">
+                        <Shows/>
+                    </div>
+                    <div data-mixcloud-play-button="/planetboo/planet-bootyshakers/">Click 4 bootyshakers</div>
+                </div>
+            </div>
+            );
+        }
     }
-  }
-
-  render() {
-    this.getUploads();
-
-    return (
-      <div className="App">
-        <div className="Content">
-          <div className="SideBar">
-            <img src={boopic} className="LogoStyle" alt="boo"/>
-            <Buttons/>
-          </div>
-          <div className="ShowBar">
-          </div>
-          <div data-mixcloud-play-button="/planetboo/planet-bootyshakers/">Click 4 bootyshakers</div>
-        </div>
-      </div>
-    );
-  }
-}
 
 export default App;
--- src/Shows.js
+++ src/Shows.js
@@ -2,8 +2,28 @@ import React, { Component } from 'react';
 import './App.css';
 
 class Shows extends Component {
+    getResponse() {
+        //so the API hook is this https://api.mixcloud.com/planetboo/cloudcasts/
+        var xmlHttp = new XMLHttpRequest();
+        var url = "https://api.mixcloud.com/planetboo/cloudcasts/";
+        xmlHttp.open("GET", url, false);
+        xmlHttp.send(null);
+        return(xmlHttp.responseText);
+    }
+
+    getUploads() {
+        var response = this.getResponse();
+        var responseParsed = JSON.parse(response);
+        var shows = responseParsed.data;
+
+        for (var i = 0; i < shows.length; i++) {
+            console.log(shows[i]);
+        }
+    }
+
     render (){
-        return;
+        this.getUploads();
+        return null;
     }
 }
 
