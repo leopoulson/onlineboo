20b7c01cfb9e0ce8ac6ce0df19e30b3408cdb4cb
Leo Poulson <lp16860@my.bristol.ac.uk>
2018-09-17 13:27:56 +0100

    add new iframe widget...

--- src/App.js
+++ src/App.js
@@ -6,8 +6,10 @@ import './App.css';
 
 class App extends Component {
     render() {
+
         return (
             <div className="App">
+            <script src="//widget.mixcloud.com/media/js/widgetApi.js" type="text/javascript"/>
                 <div className="Content">
                     <div className="SideBar">
                         <img src={boopic} className="LogoStyle" alt="boo"/>
@@ -16,6 +18,16 @@ class App extends Component {
                     <div className="ShowBar">
                         <Shows/>
                     </div>
+                </div>
+                <div name="mixcloud-widget"> 
+                    <script src="//widget.mixcloud.com/media/js/widgetApi.js" type="text/javascript"></script>
+                    <iframe id="mixcloud-iframe" title="mixcloud-iframe"/>
+                    <script type="text/javascript">
+                    var widget = Mixcloud.PlayerWidget(document.getElementById("mixcloud-iframe"));
+                    widget.ready.then(function() {
+                        // Put code that interacts with the widget here
+                    });
+                </script>
                 </div>
             </div>
             );
