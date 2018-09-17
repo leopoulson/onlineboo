40a31cd0fc06a686b24a09e4d12213f33926ac88
Leo Poulson <lp16860@my.bristol.ac.uk>
2018-09-17 13:20:53 +0100

    some changes to show items

--- src/Show.js
+++ src/Show.js
@@ -12,10 +12,11 @@ class Show extends Component {
         <div className="show" data-mixcloud-play-button={this.props.data.key}> 
             <img className="thumbnail" src={this.props.data.pictures.large}/>
             <div className="showName">
-                {this.props.data.name} 
+                <p className="showNameText">
+                    {this.props.data.name} 
+                </p>
             </div>
         </div>
-        
         );
     }
 }
